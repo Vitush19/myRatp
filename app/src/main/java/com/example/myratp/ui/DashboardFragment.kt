c60ab966d2c947e7fc48e0ec0a56f2fc3fc3e0f6
Vitush19 <49172378+Vitush19@users.noreply.github.com>
2020-04-14 03:09:43 +0100

    plan fragment done

--- app/src/main/java/com/example/myratp/ui/DashboardFragment.kt
+++ app/src/main/java/com/example/myratp/ui/DashboardFragment.kt
@@ -5,13 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 import com.example.myratp.R
 
 /**
  * A simple [Fragment] subclass.
  */
-class DashboardFragment : Fragment() {
+class DashboardFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +24,24 @@ class DashboardFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<CardView>(R.id.timetable_button).setOnClickListener(this)
+        view.findViewById<CardView>(R.id.plans_button).setOnClickListener(this)
+        view.findViewById<CardView>(R.id.favoris_button).setOnClickListener(this)
+        view.findViewById<CardView>(R.id.qr_code_button).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.timetable_button -> navController!!.navigate(R.id.action_dashboardFragment_to_timetableFragment)
+            R.id.plans_button -> navController!!.navigate(R.id.action_dashboardFragment_to_plansFragment)
+            R.id.favoris_button -> navController!!.navigate(R.id.action_dashboardFragment_to_savedFragment)
+            R.id.qr_code_button -> navController!!.navigate(R.id.action_dashboardFragment_to_qrcodeFragment)
+        }
     }
 
 }
